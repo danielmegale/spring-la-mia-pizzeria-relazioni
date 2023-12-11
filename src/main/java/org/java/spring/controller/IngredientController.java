de424acae2a16c4e1bca224c399ec0cab2705c91
danielmegale <danim1@live.it>
2023-12-11 21:39:12 +0100

    add ingredients crud

--- src/main/java/org/java/spring/controller/IngredientController.java
+++ src/main/java/org/java/spring/controller/IngredientController.java
@@ -1,8 +1,9 @@
 package org.java.spring.controller;
 
+
 import org.java.spring.db.pojo.Ingredient;
+import org.java.spring.db.pojo.Pizza;
 import org.java.spring.db.service.IngredientService;
-import org.java.spring.db.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,60 +17,60 @@ import java.util.List;
 @Controller
 public class IngredientController {
 
-	@Autowired
-	private PizzaService pizzaService;
-	
     @Autowired
     private IngredientService ingredientService;
 
-    @GetMapping
+    @GetMapping("/ingredients")
     public String listIngredients(Model model) {
         List<Ingredient> ingredients = ingredientService.findAll();
         model.addAttribute("ingredients", ingredients);
         return "ingredient-list";
     }
 
-    @GetMapping("/create")
-    public String createIngredientForm(Model model) {
-        model.addAttribute("ingredient", new Ingredient());
-        model.addAttribute("actionUrl", "/ingredients/create");
+    @GetMapping("/ingredients/create")
+    public String createIngredient(Model model) {
+        Ingredient ingredient = new Ingredient();
+        model.addAttribute("ingredient", ingredient);
         return "ingredient-form";
     }
 
-
-    @PostMapping("/create")
-    public String createIngredient(@Valid @ModelAttribute Ingredient ingredient, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "ingredient-form";
-        }
+    @PostMapping("/ingredients/create")
+    public String storeIngredient(@ModelAttribute Ingredient ingredient) {
         ingredientService.save(ingredient);
         return "redirect:/ingredients";
     }
 
-    @GetMapping("/edit/{id}")
-    public String editIngredientForm(@PathVariable int id, Model model) {
+    @GetMapping("/ingredients/edit/{id}")
+    public String editIngredient(@PathVariable int id, Model model) {
         Ingredient ingredient = ingredientService.findById(id);
         if (ingredient != null) {
             model.addAttribute("ingredient", ingredient);
             model.addAttribute("actionUrl", "/ingredients/edit/" + id);
             return "ingredient-form";
         } else {
-
             return "redirect:/ingredients";
         }
     }
 
-
-    @PostMapping("/edit/{id}")
-    public String editIngredient(@PathVariable Long id, @Valid @ModelAttribute Ingredient ingredient,
-                                BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "ingredient-form";
-        }
+    @PostMapping("/ingredients/edit/{id}")
+    public String updateIngredient(@PathVariable int id, @ModelAttribute Ingredient ingredient) {
         ingredient.setId(id);
         ingredientService.save(ingredient);
+
         return "redirect:/ingredients";
     }
 
+    
+    @PostMapping("/ingredients/delete/{id}")
+    public String deleteIngredient(@PathVariable int id) {
+        Ingredient ingredient = ingredientService.findById(id);
+        
+        for (Pizza pizza : ingredient.getPizzas()) {
+            pizza.getIngredients().remove(ingredient);
+        }
+        
+        ingredientService.delete(ingredient);
+        return "redirect:/ingredients";
+    }
 
 }
--- src/main/java/org/java/spring/db/service/IngredientService.java
+++ src/main/java/org/java/spring/db/service/IngredientService.java
@@ -23,4 +23,8 @@ public class IngredientService {
 	public void save(Ingredient ingredient) {
 		ingredientRepository.save(ingredient);
 	}
+	public void delete(Ingredient ingredient) {
+		
+		ingredientRepository.delete(ingredient);
+	}
 }
